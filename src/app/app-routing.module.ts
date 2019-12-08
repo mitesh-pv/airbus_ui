deec3adce29d799b43a6cc3b369e82821ccbd657
Shashi Priya <shashi.priya06@gmail.com>
2019-12-08 12:54:24 +0530

    addedComponents for ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListOfFlightsComponent} from './list-of-flights/list-of-flights.component';
+import { BodyComponent} from './body/body.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list-Of-Flights', component: ListOfFlightsComponent},
+  { path: 'select', component: BodyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

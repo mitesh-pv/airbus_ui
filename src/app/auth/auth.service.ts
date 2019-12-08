7a963b48131de45836c4ecefb7c068099f746320
mitesh-pv <mitesh.vishwakarma@gmail.com>
2019-12-08 10:40:03 +0530

    starters

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RegisterPayload } from './register-payload';
+import { Observable } from 'rxjs';
+import { LoginPayload } from './login-payload';
+import { map } from 'rxjs/operators'
+import { JwtAuthResponse } from '../jwt-auth-response';
+import { LocalStorageService } from 'ngx-webstorage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url =  'http://localhost:8787/';
+
+  constructor(private httpClient: HttpClient, private localStorageService: LocalStorageService) { }
+
+  register(registerPayload: RegisterPayload): Observable<any>{
+    return this.httpClient.post(this.url+'api/auth/signup', registerPayload);
+  }
+  
+  login(loginPayload: LoginPayload): Observable<boolean>{
+    return this.httpClient.post<JwtAuthResponse>(this.url+"api/auth/login", loginPayload).pipe(map(data => {
+      this.localStorageService.store('authenticationToken', data.authenticationToken);
+      this.localStorageService.store('username', data.username);
+      return true;
+    }));
+  }
+
+  isAuthenticated(): Boolean{
+    return this.localStorageService.retrieve("username") !=null 
+  }
+
+}
